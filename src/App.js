b86b82d9eae42c7725ba457920aa80363e4e6d93
Shubham <shubhamkumar1582003@gmail.com>
2024-10-08 18:07:24 +0530

    home-page-changes

--- src/App.js
+++ src/App.js
@@ -2,22 +2,19 @@ import './assets/styles/ResearchForm.css';
 import './assets/styles/Homepage.css';
 import './assets/styles/SignInForm.css';
 import './App.css';
-import ResearchForm from './components/ResearchForm';
+import Header from './components/Header';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
-import SignInForm from './components/SignInForm';
+import Slider from './components/ImageSlider';
+
 
 function App() {
   return (
     <>
-    
+       <Header/>
        <HomePage/>
-       <ResearchForm/>
-       <SignInForm/>
+       <Slider/>
        <Footer/>
-       
-
-    
     </>
   );
 }
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,33 +1,32 @@
 
 import React from 'react'
-import govlogo from '../assets/images/gov-logo.png';
+
 
 
 export default function HomePage() {
   return (
     <div>
-      <nav className="navbar bg-body-tertiary" padding="1px" border="1px">
-<div className="container" id='container-nav-1' padding="1px" margin-left="0px">
-<img src={govlogo} width="60" height="100" margin="1px">
-</img>
-<a className="navbar-brand" href="#">
-
-</a>
-</div>
-</nav>
+   
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
 <div className="container-fluid">
 <a className="navbar-brand" href="#">Home</a>
-<button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-<span className="navbar-toggler-icon"></span>
-</button>
+
 <div className="collapse navbar-collapse" id="navbarSupportedContent">
 <ul className="navbar-nav me-auto mb-2 mb-lg-0">
   <li className="nav-item">
-    <a className="nav-link active" href="#">Features</a>
+    <a className="nav-link active" href="#">ABOUT US</a>
   </li>
   <li className="nav-item">
-    <a className="nav-link" href="#">Links</a>
+    <a className="nav-link active" href="#">REEARCH</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link active" href="#">START-UPS</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link active" href="#">INOVATION</a>
+  </li>
+  <li className="nav-item">
+    <a className="nav-link active" href="#">IPR</a>
   </li>
  
 
