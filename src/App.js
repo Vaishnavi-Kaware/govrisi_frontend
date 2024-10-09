22039475df1fb099df61a0bb0c648a9abb9e340e
Shubham <shubhamkumar1582003@gmail.com>
2024-10-09 15:20:55 +0530

    complted-routing & research

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import './App.css';
-import Header from './components/Header';
+
 import Footer from './components/Footer';
-import ImageSlider from './components/ImageSlider';
+
 
 
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import HomePage from './components/HomePage'; // Import your HomePage component
 
-import Research from './components/ResearcherProfile'; // Create this component for "RESEARCH"
-
+import Researchhomepage from './components/Researchhomepage'; // Create this component for "RESEARCH"
+import SignIn from './components/SignInForm';
+import Resercherprofile from './components/ResearcherProfile';
 
 
 function App() {
   return (
     <>
-       <Header/>
+       
        <Router>
        <Routes>
         <Route path="/" element={<HomePage />} />
         
-        <Route path="/research" element={<Research />} />
+        <Route path="/research" element={<Researchhomepage />} />
+        <Route path="/signIn" element={<SignIn />} />
+        <Route path="/research-profile" element={<Resercherprofile />} />
         
       </Routes>
     </Router>
-    <ImageSlider/>
     <Footer/>
     </>
   );
